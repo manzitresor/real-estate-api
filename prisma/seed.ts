3c1381c7cc5cb5be5967482ac0c4b2b6c7fa4646
manzitresor <manzitre@gmail.com>
2025-02-27 18:51:18 +0200

    chore: configure prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function main() {
+  const user1 = await prisma.user.upsert({
+    where: { email: 'sabin@adams.com' },
+    update: {},
+    create: {
+      email: 'sabin@adams.com',
+      name: 'Sabin Adams',
+      password: 'password-sabin',
+    },
+  });
+
+  const user2 = await prisma.user.upsert({
+    where: { email: 'alex@ruheni.com' },
+    update: {},
+    create: {
+      email: 'alex@ruheni.com',
+      name: 'Alex Ruheni',
+      password: 'password-alex',
+    },
+  });
+
+  console.log({ user1, user2 });
+}
