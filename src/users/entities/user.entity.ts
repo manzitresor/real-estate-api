8492310cb6c24f07ad8f74ac84472e696454067b
manzitresor <manzitre@gmail.com>
2025-03-02 22:47:57 +0200

    chore: add business logic for users endpoint

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  email: string | null;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  published: boolean;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
